a98414ee47a365225baaa80be3ad007654ea7c63
Spencer Porter <spencer.porter3@gmail.com>
2020-12-01 13:10:27 -0500

    update to remove unnecessary route

--- app/main/views.py
+++ app/main/views.py
@@ -7,21 +7,3 @@ import requests
 def index():
     return render_template("templates/index.html")
 
-
-@main.route('/link')
-def get_link():
-
-    url = request.args.get('url')
-
-    params = {'video': True,
-              'audio': True,
-              'screenshot': False}
-
-    if request.args.get('iframe'):
-        params['iframe'] = True
-
-    headers = {'x-api-key': current_app.config['MICROLINK_API_KEY']}
-    m_url = 'https://pro.microlink.io?url={}'.format(url)
-    r = requests.get(m_url, headers=headers, params=params)
-
-    return r.json(), 200
